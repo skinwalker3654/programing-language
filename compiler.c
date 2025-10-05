895898b5bc2d3c0d00a15b0ce40256561d2bb17d
skinwalker3654 <skinrr932@gmail.com>
2025-10-05 18:59:22 +0300

    More keywords added

--- compiler.c
+++ compiler.c
@@ -1,39 +1,38 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 #include <ctype.h>
 
 typedef enum {
-    VARIABLE_VALUE,
-    VARIABLE_NAME,
-    INTEGER_KEYWORD,
-    STRING_KEYWORD,
     LET_KEYWORD,
-    COLON_CHARACTER,
-    SEMICOLON_CHARACTER,
-    ASSIGN_CHARACTER,
+    VAR_NAME,
+    COLON_CHAR,
+    INTEGER,
+    TEXT,
+    FLOAT,
+    BOOL,
+    EQUAL,
+    VALUE,
+    STRINGVALUE,
+    TRUE_,
+    FALSE_,
+    SEMICOLON,
+    EOF_T,
 } TokenType;
 
-#define NAME_SIZE 100
 typedef struct Token {
     TokenType type;
-    char name[NAME_SIZE];
+    char name[100];
 } Token;
 
-#define VALUE_SIZE 50
-typedef struct Variable {
-    TokenType type;
-    char name[NAME_SIZE];
-    char value[VALUE_SIZE];
-} Variable;
-
-Token getToken(char **input) {
-    while(**input == ' ') (*input)++;
+Token getNextToken(char **input) {
+    while(isspace(**input)) (*input)++;
 
     if(isalpha(**input)) {
         Token token;
         int counter = 0;
 
-        while(isalnum(**input)) {
+        while(isalnum(**input) || **input == '_') {
             token.name[counter] = **input;
             (*input)++;
             counter++;
@@ -44,21 +43,38 @@ Token getToken(char **input) {
             token.type = LET_KEYWORD;
             return token;
         } else if(strcmp(token.name,"integer")==0) {
-            token.type = INTEGER_KEYWORD;
+            token.type = INTEGER;
             return token;
         } else if(strcmp(token.name,"string")==0) {
-            token.type = STRING_KEYWORD;
+            token.type = TEXT;
+            return token;
+        } else if(strcmp(token.name,"float")==0) {
+            token.type = FLOAT;
+            return token;
+        } else if(strcmp(token.name,"boolean")==0) {
+            token.type = BOOL;
+            return token;
+        } else if(strcmp(token.name,"true")==0) {
+            token.type = TRUE_;
+            return token;
+        } else if(strcmp(token.name,"false")==0) {
+            token.type = FALSE_;
             return token;
         } else {
-            token.type = VARIABLE_NAME;
+            token.type = VAR_NAME;
             return token;
         }
     }
 
-    if(isdigit(**input)) {
-        Token token = {VARIABLE_VALUE,""};
+    if(isdigit(**input)||**input=='-') {
+        Token token = {VALUE};
         int counter = 0;
-        while(isdigit(**input)) {
+        if(**input == '-') {
+            token.name[counter++] = **input;
+            (*input)++;
+        }
+
+        while(isdigit(**input) || **input == '.') {
             token.name[counter] = **input;
             (*input)++;
             counter++;
@@ -69,7 +85,7 @@ Token getToken(char **input) {
     }
 
     if(**input == '"') {
-        Token token = {VARIABLE_VALUE,""};
+        Token token = {STRINGVALUE};
         int counter = 0;
 
         (*input)++;
@@ -79,118 +95,172 @@ Token getToken(char **input) {
             counter++;
         }
 
-        (*input)++;
         token.name[counter] = '\0';
-
+        (*input)++; 
         return token;
     }
 
     char operation = *(*input)++;
     switch(operation) {
-        case '=': return (Token){ASSIGN_CHARACTER,"="};
-        case ':': return (Token){COLON_CHARACTER,":"};
+        case ':': return (Token){COLON_CHAR,":"}; 
+        case '=': return (Token){EQUAL,"="};
+        case ';': return (Token){SEMICOLON,";"};
     }
 
-    return (Token){SEMICOLON_CHARACTER,""};
+    return (Token){EOF_T,""};
 }
 
-Variable parseTokens(char **input) {
-    Variable var = {0,"",""};
+typedef struct Variable {
+    TokenType type;
+    char var_name[100];
+    int int_value;
+    float float_value;
+    char stringValue[100];
+    char boolean[20];
+} Variable;
 
-    Token token = getToken(input);
+int parseTokens(Variable *var,char **input) {
+    Token token = getNextToken(input);
     if(token.type != LET_KEYWORD) {
-        printf("Error: Invalid keyword -> '%s'\n",token.name);
-        return var;
+        printf("Error: Forgot to assing the variable using 'let'\n");
+        return -1;
     }
 
-    token = getToken(input);
-    if(token.type != VARIABLE_NAME) {
-        printf("Error: Invalid variable name -> '%s'\n",token.name);
-        return var;
+    token = getNextToken(input);
+    if(token.type != VAR_NAME) {
+        printf("Error: Invalid variable name '%s'\n",token.name);
+        return -1;
     }
 
-    strcpy(var.name,token.name);
-    token = getToken(input);
-    if(token.type != COLON_CHARACTER) {
-        printf("Error: ':' <- is missing\n");
-        return var;
+    strcpy(var->var_name,token.name);
+
+    token = getNextToken(input);
+    if(token.type != COLON_CHAR) {
+        printf("Error: Forgot to assing ':' for variable values\n");
+        return -1;
     }
 
-    token = getToken(input);
-    if(token.type != INTEGER_KEYWORD && token.type != STRING_KEYWORD) {
-        printf("Error: Invalid variable type -> '%s'\n",token.name);
-        return var;
-    } else {
-        if(token.type == INTEGER_KEYWORD) {
-            var.type = INTEGER_KEYWORD;
-        } else {
-            var.type = STRING_KEYWORD;
+    token = getNextToken(input);
+    if(token.type != INTEGER &&
+      token.type != TEXT &&
+      token.type != FLOAT &&
+      token.type != BOOL) {
+        printf("Error: Invalid type '%s'\n",token.name);
+        return -1;
+    }
+
+    var->type = token.type;
+
+    token = getNextToken(input);
+    if(token.type != EQUAL) {
+        printf("Error: Forgot to assing the variable with '='\n");
+        return -1;
+    }
+
+    token = getNextToken(input);
+    if(token.type != VALUE && token.type != STRINGVALUE && token.type != FALSE_ && token.type != TRUE_) {
+        printf("Error: Invalid value '%s'\n",token.name);
+        return -1;
+    }
+
+    if(var->type == INTEGER) {
+        if(token.type == STRINGVALUE) {
+            printf("Error: Integers cannot be inside \"\"\n");
+            return -1;
         }
+
+        char *endPtr;
+        int number = strtol(token.name,&endPtr,10);
+        if(*endPtr != '\0') {
+            printf("Error: Invalid integer '%s'\n",token.name);
+            return -1;
+        }
+
+        var->int_value = number;
+    } else if(var->type == TEXT) {
+        if(token.type != STRINGVALUE) {
+            printf("Error: Strings must be inside \"\"\n");
+            return -1;
+        }
+
+        strcpy(var->stringValue,token.name);
+    } else if(var->type == FLOAT) {
+        if(token.type == STRINGVALUE) {
+            printf("Error: Floats cannot have \"\"\n");
+            return -1;
+        }
+
+        char *endPtr;
+        float number = strtof(token.name,&endPtr);
+        if(*endPtr != '\0') {
+            printf("Error: Invalid float '%s'\n",token.name);
+            return -1;
+        }
+
+        var->float_value = number;
+    } else if(token.type == FALSE_) {
+        strcpy(var->boolean,"false");
+    } else if(token.type == TRUE_) {
+        strcpy(var->boolean,"true");
+    } else {
+        printf("Error: Booleans cannot be inside \"\"\n");
+        return -1;
     }
 
-    token = getToken(input);
-    if(token.type != ASSIGN_CHARACTER) {
-        printf("Error: '=' <- is missign\n");
-        return var;
+    token = getNextToken(input);
+    if(token.type != SEMICOLON) {
+        printf("Error: Forgot a semicolon ';'\n");
+        return -1;
     }
 
-    token = getToken(input);
-    if(token.type != VARIABLE_VALUE) {
-        printf("Error: Invalid value -> '%s'\n",token.name);
-        return var;
+    token = getNextToken(input);
+    if(token.type != EOF_T) {
+        printf("Error: Invalid arguments passed\n");
+        return -1;
     }
 
-    strcpy(var.value,token.name);
-    token = getToken(input);
-    if(token.type != SEMICOLON_CHARACTER) {
-        printf("Error: Forgot the '%s' at the end\n",token.name);
-        return var;
-    }
-
-    return var;
+    return 0;
 }
 
-int main(int argc,char **argv) {
+void codeGen(FILE *file,Variable *var) {
+    if(var->type == INTEGER) {
+        fprintf(file,"    int %s = %d;\n",var->var_name,var->int_value);
+    } else if(var->type == TEXT) {
+        fprintf(file,"    char *%s = \"%s\";\n",var->var_name,var->stringValue);
+    } else if(var->type == FLOAT) {
+        fprintf(file,"    float %s = %f;\n",var->var_name,var->float_value);
+    } else if(strcmp(var->boolean,"true")==0 || strcmp(var->boolean,"false")==0) {
+        fprintf(file,"    bool %s = %s;\n",var->var_name,var->boolean);
+    }
+}
+
+int main(int argc,char *argv[]) {
     if(argc != 2) {
-        printf("Error: Invalid usage\n");
-        printf("Correct usage: ./main <file_name>\n");
+        printf("Error: Invalid arguments count\n");
+        printf("Usage: ./main <file_name>\n");
         return 1;
     }
 
-    char line[NAME_SIZE];
-    FILE *file1 = fopen(argv[1],"r");
-    if(file1 == NULL) {
+    Variable var;
+    FILE *input = fopen(argv[1],"r");
+    if(!input) {
         printf("Error: Failed to open the file\n");
         return 1;
     }
 
-    
-    FILE *file2 = fopen("out.c","w");
-    if(!file2) {
-        printf("Error: Failed to open the file\n");
-        fclose(file1);
-        return 1;
-    }
+    FILE *file = fopen("output.c","w");
+    fprintf(file,"#include <stdio.h>\n#include <stdbool.h>\nint main(void) {\n");
 
-    fprintf(file2,"#include <stdio.h>\n");
-    fprintf(file2,"\nint main(void) {\n");
-
-    while(fgets(line,sizeof(line),file1)) {
+    char line[100];
+    while(fgets(line,sizeof(line),input)) {
         char *ptr = line;
-        Variable var = parseTokens(&ptr);
-
-        if(var.type == INTEGER_KEYWORD) {
-            fprintf(file2,"    int %s = %s;\n",var.name,var.value);
-        } else if(var.type == STRING_KEYWORD) {
-            fprintf(file2,"    char *%s = \"%s\";\n",var.name,var.value);
-        }
+        int check = parseTokens(&var,&ptr);
+        if(check == -1) { remove("output.c"); return 1; }
+        codeGen(file,&var);
     }
 
-    fprintf(file2,"    return 0;\n");
-    fprintf(file2,"}\n");
-
-    fclose(file1);
-    fclose(file2);
+    fprintf(file,"    return 0;\n}\n");
+    fclose(file);
 
     return 0;
 }
